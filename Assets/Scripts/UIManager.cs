a343b1cb23eb02353ffc15a1b1c28a471c805ef6
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-05-26 17:10:24 +0200

    imported standard assets and rain particle effect

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,6 +14,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        resourceData.ResetData();
         UpdateText();
     }
 
