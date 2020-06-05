48561474f64b723e20962ed4638f65e4fabe263c
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-06-05 16:26:41 +0200

    added new models for gatherer and refiner building

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -21,6 +21,7 @@ public class UIManager : Singleton<UIManager>
     // Start is called before the first frame update
     void Start()
     {
+        //_mainMenu.gameObject.SetActive(true);
         resourceData.ResetData();
         InvokeRepeating("UpdateText", 0f, 2f);
         UpdateText();
