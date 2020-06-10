3af11ae4a4c7955c67295a6068c16046e4508332
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-06-10 22:57:20 +0200

    added space ship wioth rocket exhaust animations and blendtree with animations

---
+++ Assets/Scripts/ExhaustController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExhaustController : MonoBehaviour
+{
+    [SerializeField] ParticleSystem[] exhaustRockets = new ParticleSystem[4];
+    public float weight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weight = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            weight += 0.3f;
+        }
+
+        EmitRockets();
+    }
+
+    void EmitRockets()
+    {
+        for (int i = 0; i < exhaustRockets.Length; i++)
+        {
+            exhaustRockets[i].Emit((int)weight);
+            
+        }
+
+        weight -= 0.2f;
+        weight = Mathf.Clamp(weight, 1f, 500f);
+
+
+    }
+}
---
+++ Assets/Scripts/ShipCharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCharacterController : MonoBehaviour
+{
+    Animator animator;
+    ExhaustController rocketController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rocketController = GetComponentInChildren<ExhaustController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateAnimation();
+        //Set movement based on horizontal and vertikal input
+    }
+
+    public void Move()
+    {
+        
+    }
+
+    void UpdateAnimation()
+    {
+        float blendWeight;
+        blendWeight = rocketController.weight;
+        blendWeight = (blendWeight - 1) / (5 - 1);
+        //blendWeight *= 100f;
+        Mathf.Clamp(blendWeight, 0f, 1f);
+        animator.SetFloat("Blend", blendWeight);
+    }
+}
