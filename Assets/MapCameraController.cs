2890bd3fb65321123f080e65e0fc32950c38bc7c
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-09-26 22:36:11 +0200

    forgot to commit a lot of shit

---
+++ Assets/MapCameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCameraController : MonoBehaviour
+{
+    [SerializeField] float movementTime = 2;
+    [SerializeField] float zoomTime = 2;
+
+    [SerializeField] Transform cameraTransform;
+
+    
+    Vector2 oldMousePosition = Vector2.zero;
+    Vector2 mousePosition;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMouseInput();
+    }
+
+    void HandleMouseInput()
+    {
+
+        if (Input.GetButton("Fire1"))
+        {
+            oldMousePosition = mousePosition;
+            mousePosition = Input.mousePosition;
+            cameraTransform.position += new Vector3(-Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"),  0f);
+            //Get mouseDelta
+            //Move camera a certain amount depending on this delta
+        }
+    }
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    
+
+    void OpenMissionMeny()
+    {
+        //Toogle MenuPrefab to active
+        //Set position to the same as node
+        //load data for what should be shown
+    }
+
+    void OnMouseDown()
+    {
+        OpenMissionMeny();
+    }
+
+    
+
+
+}
